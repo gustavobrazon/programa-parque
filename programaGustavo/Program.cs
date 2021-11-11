63e06547a09ed003057a6d5a6613ef3da25da666
gustavobrazon <92831689+gustavobrazon@users.noreply.github.com>
2021-11-11 13:39:58 -0400

    Subir Programa

---
+++ programaGustavo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace programaGustavo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edadMinima = 15;
+            double estaturaMinima = 1.65; 
+
+            Console.WriteLine("Ingresar tu edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingresar tu estatura en metros: ");
+            double estatura = Convert.ToDouble(Console.ReadLine());
+
+            bool edadPermitida = (edad >= edadMinima);
+            bool estaturaPermitida = (estatura >= estaturaMinima);
+
+            if(edadPermitida == true) {
+                if(estaturaPermitida == true) {
+                    Console.WriteLine("Puedes entrar al parque");
+                } else {
+                    Console.WriteLine("Tu estatura no es suficiente para entrar al parque");
+                }
+            } else {
+                Console.WriteLine("Tu edad no es suficiente para entrar al parque");
+            }
+        }
+    }
+}
